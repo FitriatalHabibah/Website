3416e5a091ce74763ff9feae902fffff1fe4baf4
Fitriatal Habibah <148746611+FitriatalHabibah@users.noreply.github.com>
2024-11-22 00:28:12 +0700

    Add files via upload

---
+++ codelabweb4/app/Config/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace app\Config;
+
+class DatabaseConfig
+{
+    // Setting database
+    public $host = "127.0.0.1";
+    public $user = "root";
+    public $password = "root";
+    public $database_name = "praktikumweb";
+    public $port = 8889;
+}
